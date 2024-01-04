c2d971ee8f511255f0028bfc0ca3bfe7591bc918
diogofmaciel <36400930+diogofmaciel@users.noreply.github.com>
2024-01-04 19:13:33 +0800

    Add contributions for days 001 to 007

--- replit-100-days-of-code-python/day-006/main.py
+++ replit-100-days-of-code-python/day-006/main.py
@@ -1,13 +1,46 @@
-print("""Though shalt not pass - Unless""")
-print()
-username = input('Username > ')
-password = input('Password > ')
-if username == 'boris' and password == 'mummy':
-  print('Welcome back, Mr {username}!')
-elif username == 'honor' and password == 'Gal0rE':
-  print('Welcome back, Dr Gale!')
-elif username == 'diana' and password == 'umbr3LLa':
-  print('Welcome back, Ms Peel!')
-else:
-  print('No way!')
+# print("SECURE LOGIN")
+# username = input("Username > ")
+# password = input("Password> ")
 
+# if username == "mark" and password == "password":  
+# print("Welcome Mark!")
+# elif username == "suzanne" and password == "Su74nne":
+# print("Hey there Suzanne!")
+# else:
+# print("Go away!")
+
+# print("SECURE LOGIN")
+# username = input("Username > ")
+
+# if username == "mark":
+# print("Welcome Mark!")
+# elif username == "suzanne":
+# print("Hey there Suzanne!")
+# else:
+# print("Go away!")
+
+# season = input("what is your favorite season?")
+# if season == "spring":
+# print("Ah! The birds are chirping and flowers blooming.")
+# elif season == "summer": 
+# print("Catch some sun and cool off with a lemonade.")
+# elif season == "autumn":
+# print("The leaves are changing and the air is crisp. Enjoy!")
+# elif season == "winter":
+# print("Stay warm by the fire and watch the snow fall.")
+# else: 
+# print("I don't know that season. Please try again.")
+
+print("Login to my page!")
+print()
+username = input("Username > ")
+password = input("Password >")
+print()
+if username == "diogofmaciel" and password == "123456":
+  print("Hello!")
+elif username == "gabmaciel" and password == "654321":
+  print("Hello!")
+elif username == "danmaciel" and password == "321654":
+  print("Hello!")
+else:
+  print("Fuck off!")
