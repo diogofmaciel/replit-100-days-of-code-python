0ad18ecf0f7b1031598b300a043eebba0d8b67ef
diogofmaciel <36400930+diogofmaciel@users.noreply.github.com>
2024-01-10 21:10:14 +0800

    Add contributions for days 001 to 007

--- replit-100-days-of-code-python/day-007/main.py
+++ replit-100-days-of-code-python/day-007/main.py
@@ -1,28 +1,71 @@
-INTRO = """=== Fake Fan Question Generator ===
+# tvShow = input("What is your favorite tv show? ")
+# if tvShow == "Succession":
+# print("Cool!")
+# faveCharacter = input("Who is your favorite character? ")
+# if faveCharacter == "Kendall":
+# print("Right answer")
+# else:
+# print("Nah, Kendall's the greatest")
+# elif tvShow == "The Boys":
+# print("Aww, sad times")
+# else:
+# print("Yeah, that's cool and all…")
 
-Let's see if you're just pretending
+# tvShow = input("What is your favourite tv show? ")
+# if tvShow == "peppa pig":
+# print("Ugh, why?")
+# faveCharacter = input("Who is your favourite character? ")
+# if faveCharacter == "daddy pig":
+# print("Right answer")
+# else:
+# print("Nah, Daddy Pig's the greatest")
+# elif tvShow == "paw patrol":
+# print("Aww, sad times")
+# else:
+# print("Yeah, that's cool and all…")
 
-"""
+# order = input("What would you like to order: pizza or hamburger? ")
+# if order == "hamburger":
+# print("Thank you.")
+# cheese = input("Do you want cheese?")
+# if cheese == "yes":
+# print("You got it.")
+# else: 
+# print("No cheese it is.")
+# elif order == "pizza":
+# print("Pizza coming up.")
+# toppings = input("Do you want pepperoni on that?")
+# if toppings == "yes":
+# print("We will add pepperoni.")
+# else:
+# print("Your pizza will not have pepperoni.")
+# else:
+# print("Sorry, we don't have that.")
 
-print(INTRO)
-favshow = input('What is your favourite tv show?: ')
+print("Succession Checker")
 print()
-if favshow == 'dr who':
-  print('Oh my, that\'s exciting!')
-  favdoctor = input('What is your favourite doctor then?: ')
-  if favdoctor == 'peter davison':
-    print('Come on, any bloke off a vet series could have done better!')
-  elif favdoctor == 'david tennant':
-    print('Now you\'re talking!')
-    favcomp = input('What is your favourite companion with the tenth doctor?: ')
-    if favcomp == 'martha':
-      print('I heard a lot of people saying that.')
-    elif favcomp == 'astrid': 
-      print('She was a one-off, but I really enjoyed her as well.')
-    elif favcomp == 'donna': 
-      print('Agreed! She was awesome!')
-    
-  else:
-    print('Yeah, that was somewhat ok-ish...')
+
+print("Let's test your knowledge of succession!")
+print()
+
+surname = input("What is the owner of Waystar's last name? ")
+if surname == "Roy":
+  print("Very good!")
 else:
-  print('Well... Enjoy...')
+  print("No good!")
+
+seasons = input("How many seasons of Succession there has been so far?")
+if seasons == "4" or "four":
+  print("Very good!")
+else:
+  print("Wrong!")
+
+faveCharacter = input("Who is your favorite character? ")
+if faveCharacter == "Kendall":
+  print("Right answer")
+elif faveCharacter == "Shiv":
+  print("She is cool, but not the greatest")
+elif faveCharacter == "Roman":
+  print("Ugh no!")
+else:
+  print("You are just wrong!")
